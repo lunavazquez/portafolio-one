673885f3987a660308bcef72a96a6cb82ceb8619
Mextli <luna.vazquezv@gmail.com>
2018-11-28 02:29:43 -0600

    Se agrega mi nombre

--- src/App.js
+++ src/App.js
@@ -8,17 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Portafolio de Mextli Vazquez</h1>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Frontend Developer
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
