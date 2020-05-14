eab3dddb7712cdb120694c19a7e1f3834f10a14e
Mextli Vazquez <luna@MacBook-Pro-de-Mextli.local>
2020-05-14 02:41:10 -0500

    Updates

---
+++ precache-manifest.2fc04a2a4794e0e6a316156bc32782e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7fd8aaf041f48e38ec1",
+    "url": "/static/css/main.241a7a3e.chunk.css"
+  },
+  {
+    "revision": "b7fd8aaf041f48e38ec1",
+    "url": "/static/js/main.b7fd8aaf.chunk.js"
+  },
+  {
+    "revision": "7ec997cdf190acb55e81",
+    "url": "/static/js/1.7ec997cd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e256337c73718594b4cb81dccce7094",
+    "url": "/static/media/mextli.6e256337.jpg"
+  },
+  {
+    "revision": "0a864fefb2e11df5fc394274c7e3e1e0",
+    "url": "/index.html"
+  }
+];
